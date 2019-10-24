87de98092039cad37a3bebf2972d7d0b12bf9bec
HMGDCR <1939376072@qq.com>
2019-10-24 09:14:39 +0800

    hmg3

--- vue.config.js
+++ vue.config.js
@@ -9,9 +9,9 @@
 
 module.exports = {
     // 资源路径 
-    publicPath: '/web04/hemugao-maizuo/dist/',
+    publicPath: '/web04/hemugao-fresh/dist/',
     // 打包路径
-    //outputDir: 'hemugao',
+    outputDir: 'hemugao',
     // 关闭eslint检查
     lintOnSave: false
 }
